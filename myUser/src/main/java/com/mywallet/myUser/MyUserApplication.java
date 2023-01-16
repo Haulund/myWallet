e82fa607e2f7034e229a3d2ac50ee5dc173883fb
Haulund <steffen.haulund@gmail.com>
2023-01-16 11:22:23 +0100

    first commit

---
+++ myUser/src/main/java/com/mywallet/myUser/MyUserApplication.java
@@ -0,0 +1,13 @@
+package com.mywallet.myUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUserApplication.class, args);
+	}
+
+}
---
+++ myUser/src/test/java/com/mywallet/myUser/MyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywallet.myUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
